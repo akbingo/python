fda4ecb7054969436e1114df1e300aa310145c5d
Akira <ideapocket@hotmail.com>
2016-11-15 10:36:57 +0800

    条件判断

--- test.py
+++ test.py
@@ -3,3 +3,17 @@
 
 print(("S1"))
 print(("S1",))
+
+height=1.75
+weight=80.5
+BMI=weight/(height*height)
+if BMI<18.5:
+    print("过轻")
+elif 18.5<=BMI<25:
+    print("正常")
+elif 25<=BMI<28:
+    print("过重")
+elif 28<=BMI<32:
+    print("肥胖")
+else:
+    print("严重肥胖")
