790f90adbfbe4a4bdfea89ececbe3fc553fae091
je <ideapocket@hotmail.com>
2017-01-22 10:21:26 +0800

    random用法

---
+++ lottery.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+def akb():
+    ske=[]
+    while len(ske)<6:
+        nmb=random.randint(1,33)
+        hkt=0
+        if len(ske)>=1:
+            for av in range(len(ske)):
+                if nmb==ske[av]:
+                    hkt=1
+        if hkt==0:
+            ske.append(nmb)
+    return sorted(ske)+["蓝球",random.randint(1,16)]
+if __name__=="__main__":
+	ngt=akb()	
+	print("%d,%d,%d,%d,%d,%d+%d"%(ngt[0],ngt[1],ngt[2],ngt[3],ngt[4],ngt[5],ngt[7]))
+		
---
+++ password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import string
+length=int(input("请输入你需要的密码位数："))
+ske=input("请输入你需要的密码形式（如需要全数字则输入1，输入其它则是数字、大小写英文字母和特殊符号的密码形式）：")
+def akb():
+    if ske=="1":
+        return ''.join([random.choice(string.digits) for av in range(length)])
+    else:
+        chrs=string.ascii_letters+string.digits+"!@#$%^&*()"
+        return ''.join([random.choice(chrs) for av in range(length)])
+if __name__=="__main__":
+    for av in range(10):
+        print(akb())
